245869dffa728944f2198be7760631c253400997
Anton Damar <ferrelhandoyo@gmail.com>
2026-02-03 11:45:58 -0500

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/home-folder/Home';
 import AwardTimeline from './components/home-folder/AwardTimeline';
